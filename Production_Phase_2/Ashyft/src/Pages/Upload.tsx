86b7256666b07efbfb8ea716a813ef8c77e6a50f
Anshuman Pattnaik <helloanshu04@gmail.com>
2025-07-14 00:22:37 +0530

    Production Phase 3 initial

--- Production_Phase_2/Ashyft/src/Pages/Upload.tsx
+++ Production_Phase_2/Ashyft/src/Pages/Upload.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 // import FileUpload from '../Sections/FileUpload'
+import FilePondUploader from '@/components/FilePondUploader';
 
 const Upload: React.FC = () => {
   return (
     <div className='bg-gray-500 w-full h-full'>
       <h1 className='text-6xl justify-self-center text-emerald-600'>From Home</h1>
       {/* <FileUpload/> */}
+      <FilePondUploader/>
     </div>
   )
 }
--- Production_Phase_2/Ashyft/src/main.tsx
+++ Production_Phase_2/Ashyft/src/main.tsx
@@ -4,6 +4,10 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
+
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
