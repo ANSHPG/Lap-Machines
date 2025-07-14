115f2732e99fe9d46f06a9cb0abd0ca5f63910f5
Anshuman Pattnaik <helloanshu04@gmail.com>
2025-07-14 16:30:08 +0530

    Production Phase 3 upload Final lap

--- Production_Phase_2/Ashyft/src/Pages/Upload.tsx
+++ Production_Phase_2/Ashyft/src/Pages/Upload.tsx
@@ -4,10 +4,13 @@ import FilePondUploader from '@/components/FilePondUploader';
 
 const Upload: React.FC = () => {
   return (
-    <div className='bg-gray-500 w-full h-full'>
-      <h1 className='text-6xl justify-self-center text-emerald-600'>From Home</h1>
+    <div  className='bg-[#1f1f1f] w-full h-full flex flex-col items-center justify-center overflow-hidden'>
+
+      
+      
       {/* <FileUpload/> */}
       <FilePondUploader/>
+
     </div>
   )
 }
--- Production_Phase_2/Ashyft/src/Sections/Canvas.tsx
+++ Production_Phase_2/Ashyft/src/Sections/Canvas.tsx
@@ -8,7 +8,7 @@ import Upload from '../Pages/Upload';
 const Canvas: React.FC = () => {
   return (
     <div id='canvas' className="bg-black rounded-[23px] overflow-auto w-full h-full text-amber-300">
-      <h1 className='bg-emerald-600 text-black flex items-center justify-center text-5xl'>From canvas</h1>
+      <h1 className='bg-emerald-600 text-black flex items-center justify-center text-5xl hidden'>From canvas</h1>
       <Routes>
         <Route path='/home' element={<Home/>}/>
         <Route path='/upload' element={<Upload/>}/>
