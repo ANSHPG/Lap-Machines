52822d73a06b8572a69bbd118475ac4659288ef1
Anshuman Pattnaik <helloanshu04@gmail.com>
2025-07-14 21:20:49 +0530

    Production Phase 4 initial

--- Production_Phase_2/Ashyft/src/Pages/Download.tsx
+++ Production_Phase_2/Ashyft/src/Pages/Download.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import DataTable from '../Sections/DataTable'
 const Download: React.FC = () => {
   return (
-    <div className='bg-emerald-500 w-full h-full flex items-center justify-center'>
-      <h1 className='text-7xl text-black font-[Formula1]'>From Download</h1>
+    <div className='bg-black w-full h-full flex items-center justify-center p-[3vw]'>
+      <h1 className='text-7xl text-black font-[Formula1] hidden'>From Download</h1>
+      <DataTable/>
     </div>
   )
 }
---
+++ Production_Phase_2/Postgres/file_manager.md
@@ -0,0 +1,21 @@
+## windows
+```sql
+psql -U postgres
+CREATE DATABASE file_manager
+\! cls
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE files (
+    file_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    size BIGINT NOT NULL,
+    location TEXT NOT NULL,
+    uploaded_at TIMESTAMP DEFAULT NOW(),
+    description TEXT
+);
+
+ALTER TABLE files
+DROP COLUMN description;
+```
